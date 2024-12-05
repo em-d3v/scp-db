dfa1f7096b813251d8deb19407290467665cf4c3
evm96m@umsystem.edu <em_dev_9836@outlook.com>
2024-12-05 17:21:40 -0600

    created database project

---
+++ db-insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO Department(ID,CODE,TITLE) VALUES
+(0,"ALC","Alchemy Department"),
+(1,"AEED","Anomalous Enitity Engagement Division"),
+(2,"ANTD","Antimemetics Division"),
+(3,"AIAD","Artificial Intelligence Applications Division"),
+(4,"CRD","Cryptozoology Division"),
+(5,"DCOM","Decommissioning Department"),
+(6,"DEL","Department of Deletions"),
+(7,"ENT","Department of Entomology"),
+(8,"ETTRA","Emergent Threat Tactical Response Authority"),
+(9,"DOFS","Fire Suppression Department"),
+(10,"DOMF","Department of Mythology and Folkloristics"),
+(11,"DOMC","Department of Miscommunications"),
+(12,"DOP","Pataphysics Department"),
+(13,"DOO","Department of Other"),
+(14,"DOPL","Department of Procurement and Liquidation"),
+(15,"DOS","Department of Surrealistics"),
+(16,"DOTT","Department of Tactical Theology"),
+(17,"TMO","Telecommunications Monitoring Office"),
+(18,"DOTA","Temporal Anomalies Department"),
+(19,"DOUN","Department of Unreality"),
+(20,"DOAB","Department of Adnormalities");
+INSERT INTO Location() VALUES
+;
+INSERT INTO Clearance(ID, LevelDesc) VALUES
+(0, 'For Official Use Only'),
+(1, 'Level 1','Confidential'),
+(2, 'Level 3','Restricted'),
+(3, 'Level 3','Secret'),
+(4, 'Level 4','Top Secret'),
+(5, 'Level 5','Thaumiel');
+
+INSERT INTO PersonnelClass()VALUES
+('A',"those deemed essential to Foundation strategic operations, and are not allowed direct access to anomalies under any circumstances"),
+('B',""),
+('C',""),
+('D',""),
+('E',""),
+('?',"UNKNOWN");
+
---
+++ insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO Department(ID,CODE,TITLE) VALUES
+(0,"ALC","Alchemy Department"),
+(1,"AEED","Anomalous Enitity Engagement Division"),
+(2,"ANTD","Antimemetics Division"),
+(3,"AIAD","Artificial Intelligence Applications Division"),
+(4,"CRD","Cryptozoology Division"),
+(5,"DCOM","Decommissioning Department"),
+(6,"DEL","Department of Deletions"),
+(7,"ENT","Department of Entomology"),
+(8,"ETTRA","Emergent Threat Tactical Response Authority"),
+(9,"DOFS","Fire Suppression Department"),
+(10,"DOMF","Department of Mythology and Folkloristics"),
+(11,"DOMC","Department of Miscommunications"),
+(12,"DOP","Pataphysics Department"),
+(13,"DOO","Department of Other"),
+(14,"DOPL","Department of Procurement and Liquidation"),
+(15,"DOS","Department of Surrealistics"),
+(16,"DOTT","Department of Tactical Theology"),
+(17,"TMO","Telecommunications Monitoring Office"),
+(18,"DOTA","Temporal Anomalies Department"),
+(19,"DOUN","Department of Unreality"),
+(20,"DOAB","Department of Adnormalities");
+INSERT INTO Location(
